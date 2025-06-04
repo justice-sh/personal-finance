6573f70bec6e3c809c97a0e3a40131bebfb8e3a4
Justice <sherllyj191@gmail.com>
2025-06-04 06:39:29 +0100

    Added font-family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,20 @@ export default function Home() {
         ipsa sequi architecto praesentium obcaecati, alias iste omnis fugit sapiente aliquid impedit facilis est, enim doloremque corporis
         fuga id?
       </p>
+      <p className="text-preset-4-bold">
+        Text preset 4 Bold: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus magnam, corporis, nam aliquid dolore
+        consequatur nisi ab, hic consectetur enim sapiente quidem quibusdam deleniti quae debitis. Expedita iusto tempora harum molestias
+        ipsa sequi architecto praesentium obcaecati, alias iste omnis fugit sapiente aliquid impedit facilis est, enim doloremque corporis
+        fuga id?
+      </p>
       <p className="text-preset-5">
         Text preset 5: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus magnam, corporis, nam aliquid dolore
+        consequatur nisi ab, hic consectetur enim sapiente quidem quibusdam deleniti quae debitis. Expedita iusto tempora harum molestias
+        ipsa sequi architecto praesentium obcaecati, alias iste omnis fugit sapiente aliquid impedit facilis est, enim doloremque corporis
+        fuga id?
+      </p>
+      <p className="text-preset-5-bold">
+        Text preset 5 Bold: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus magnam, corporis, nam aliquid dolore
         consequatur nisi ab, hic consectetur enim sapiente quidem quibusdam deleniti quae debitis. Expedita iusto tempora harum molestias
         ipsa sequi architecto praesentium obcaecati, alias iste omnis fugit sapiente aliquid impedit facilis est, enim doloremque corporis
         fuga id?

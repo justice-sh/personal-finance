b648f2ec3def29959c527b623f0d31bc64b5e1bf
Jumi525 <jumiklein525@gmail.com>
2025-06-14 11:17:52 +0100

    for testing

--- src/app/(website)/budgets/layout.tsx
+++ src/app/(website)/budgets/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 type Props = { children: React.ReactNode }
-
+//this is the layout for the budgets page
 const layout = ({ children }: Props) => {
   return <div>{children}</div>
 }

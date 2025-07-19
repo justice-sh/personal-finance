122d61283824109f5f2e7ddc01798f4035c15db5
Justice <sherllyj191@gmail.com>
2025-07-19 22:09:17 +0100

    Improved logo.tsx

--- src/shared/components/logo.tsx
+++ src/shared/components/logo.tsx
@@ -3,21 +3,38 @@ import Link from "next/link"
 import { cn } from "../lib/utils"
 
 type Props = {
+  href?: string
   className?: string
   state?: "collapsed" | "expanded"
-  href?: string
 }
 
 const Logo = ({ className, state = "expanded", href = "/" }: Props) => {
   const styles = {
-    reveal: "opacity-0 transition-all data-[reveal=true]:opacity-100 absolute duration-500 h-auto",
+    container: state === "collapsed" ? "size-[18px]" : "w-[121px] h-[22px]",
+    imageReveal: "opacity-0 transition-all data-[reveal=true]:opacity-100 absolute duration-500",
   }
 
   return (
-    <Link href={href} className={cn("block overflow-hidden transition-all duration-300", className)}>
-      <Image src="/logo-short.svg" alt="Logo" width={18} height={20} data-reveal={state === "collapsed"} className={styles.reveal} />
-      <Image src="/logo.svg" alt="Logo" width={121} height={22} data-reveal={state === "expanded"} className={styles.reveal} />
-    </Link>
+    <div className={cn("transition-all duration-300", className)}>
+      <Link href={href} className={cn("relative flex h-full w-full items-center justify-center overflow-hidden", styles.container)}>
+        <Image
+          src="/logo-short.svg"
+          alt="Logo"
+          width={18}
+          height={18}
+          data-reveal={state === "collapsed"}
+          className={cn("h-[18px]", styles.imageReveal)}
+        />
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={121}
+          height={22}
+          data-reveal={state === "expanded"}
+          className={cn("h-auto", styles.imageReveal)}
+        />
+      </Link>
+    </div>
   )
 }
 

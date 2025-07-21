e83f39ab65b5fc86f45b834e4b7c1ac30b868daf
Justice <sherllyj191@gmail.com>
2025-07-21 07:56:38 +0100

    Login api integrated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,14 +6,15 @@ import { useForm } from "@tanstack/react-form"
 import { routes } from "@/shared/constants/routes"
 import { formValidator } from "@/shared/utils/form"
 import { Form } from "@/shared/components/form/form"
+import { authAPI } from "@/shared/services/apis/auth"
 import { Button } from "@/shared/components/ui/button"
 import { PasswordSchema } from "@/shared/schemas/password"
 import { AuthLayer } from "@/shared/components/auth-layer"
+import { getErrorMessage } from "@/shared/utils/error-util"
 import { InputField } from "@/shared/components/form/input-field"
 
 const schema = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.object({ value: z.email("Invalid email address") }),
+  email: z.email("Invalid email address"),
   password: PasswordSchema(),
 })
 
@@ -23,26 +24,29 @@ export default function LoginPage() {
   const router = useRouter()
 
   const form = useForm({
-    defaultValues: {} as FormData,
+    defaultValues: { email: "test@tmail.com", password: "TestPass#1" } as FormData,
     validators: { onChange: formValidator(schema) },
-    onSubmit: (data) => {
-      console.log("Form submitted with data:", data.value)
-      router.push(routes.overview)
+    onSubmit: async (data) => {
+      try {
+        await authAPI.login(data.value)
+        router.push(routes.overview)
+      } catch (error) {
+        console.error("Login failed:", getErrorMessage(error))
+      }
     },
   })
 
   return (
     <AuthLayer title="Login" footer={{ text: "Need to create an account?", action: "Sign Up", href: routes.register }}>
       <Form className="grid gap-5">
-        <form.Field name="name" children={(field) => <InputField field={field} label="Name" placeholder="John Doe" />} />
-        <form.Field name="email.value" children={(field) => <InputField field={field} label="Email" placeholder="you@mail.com" />} />
+        <form.Field name="email" children={(field) => <InputField field={field} label="Email" placeholder="you@mail.com" />} />
         <form.Field name="password" children={(field) => <InputField field={field} label="Password" placeholder="******" />} />
 
         <form.Subscribe
-          selector={(state) => ({ isValid: state.isValid && state.isDirty, isSubmitting: state.isSubmitting })}
-          children={(state) => {
+          selector={({ isValid, isSubmitting }) => ({ isValid, isSubmitting })}
+          children={({ isValid, isSubmitting }) => {
             return (
-              <Button type="submit" size="lg" disabled={!state.isValid} isLoading={state.isSubmitting} onClick={form.handleSubmit}>
+              <Button type="submit" size="lg" disabled={!isValid} isLoading={isSubmitting} onClick={form.handleSubmit}>
                 Login
               </Button>
             )

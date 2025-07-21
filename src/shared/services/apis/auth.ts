bf89fb8d34969ee8a43cf9b2d7882aa772fcef85
Justice <sherllyj191@gmail.com>
2025-07-21 07:56:14 +0100

    auth api service

---
+++ src/shared/services/apis/auth.ts
@@ -0,0 +1,27 @@
+import type { User } from "@/shared/types/user"
+import publicClient from "../api-client/public-client"
+import securedClient from "../api-client/secured-client"
+
+const login = async (data: { email: string; password: string }) => {
+  const resp = await publicClient.post<{ accessToken: string }>("/auth", data)
+  return resp.data
+}
+
+const signInWithProvider = async (provider = "google") => {
+  const resp = await publicClient.post<string>(`/auth/oauth/${provider}`)
+  return resp.data
+}
+
+const getUser = async () => {
+  const resp = await securedClient.post<User>("/auth/me")
+  return resp.data
+}
+
+const logout = async () => publicClient.post("/auth/logout")
+
+export const authAPI = {
+  login,
+  logout,
+  getUser,
+  signInWithProvider,
+}

1f49ba55a4fbcf906bba957df562579ea7c7b25c
Justice <sherllyj191@gmail.com>
2025-07-24 10:34:33 +0100

    Refactor

--- src/shared/data/user.data.ts
+++ src/shared/data/user.data.ts
@@ -3,11 +3,12 @@ import { useQuery } from "@tanstack/react-query"
 import { queryClient } from "../lib/ts-query-client"
 
 const queryKey = ["user-data"]
+const queryFn = authAPI.getUser
 
 export const useUserData = () => {
-  return useQuery({ queryKey, queryFn: authAPI.getUser, refetchInterval: 1000 * 10 })
+  return useQuery({ queryKey, queryFn, refetchInterval: 1000 * 10 })
 }
 
 export const clearUserData = () => queryClient.removeQueries({ queryKey })
 
-export const prefetchUserData = () => queryClient.prefetchQuery({ queryKey, queryFn: authAPI.getUser })
+export const prefetchUserData = () => queryClient.prefetchQuery({ queryKey, queryFn })

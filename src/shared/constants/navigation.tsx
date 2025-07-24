db7bef6b319e90f71a2e8a001dcf3f72fa93e6cb
Justice <sherllyj191@gmail.com>
2025-07-24 10:26:22 +0100

    Exclude logout from mobile nav

--- src/shared/constants/navigation.tsx
+++ src/shared/constants/navigation.tsx
@@ -1,42 +1,42 @@
 import { routes } from "./routes"
+import { Logout } from "../components/logout"
 import ReceiptIcon from "@/shared/icons/receipt"
 import JarFillIcon from "@/shared/icons/jar-fill"
 import HouseIcon from "@/shared/icons/house-icon"
 import ChartDonutIcon from "@/shared/icons/chart-donut"
 import { NavigationItemProps } from "../types/navigation"
 import ArrowsDownUpIcon from "@/shared/icons/arrows-down-up"
-import { Logout } from "../components/logout"
 
-export const navigationItems: NavigationItemProps[] = [
+export const navigationItems = [
   {
-    title: "Overview",
+    title: "Overview" as const,
     url: routes.overview,
     Icon: HouseIcon,
   },
   {
-    title: "Transactions",
+    title: "Transactions" as const,
     url: routes.transactions,
     Icon: ArrowsDownUpIcon,
   },
   {
-    title: "Budgets",
+    title: "Budgets" as const,
     url: routes.budgets,
     Icon: ChartDonutIcon,
   },
   {
-    title: "Pots",
+    title: "Pots" as const,
     url: routes.pots,
     Icon: JarFillIcon,
   },
   {
-    title: "Recurring bills",
+    title: "Recurring bills" as const,
     url: routes.recurringBills,
     Icon: ReceiptIcon,
   },
   {
-    title: "Logout",
+    title: "Logout" as const,
     Element: Logout,
     Icon: ReceiptIcon,
     url: "nothing-to-see-here",
   },
-]
+] satisfies NavigationItemProps[]
--- src/widgets/mobile-navigation.tsx
+++ src/widgets/mobile-navigation.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import NavigationItem from "@/shared/components/navigation-item"
-import { navigationItems } from "@/shared/constants/navigation"
 import { cn } from "@/shared/lib/utils"
+import { navigationItems } from "@/shared/constants/navigation"
+import NavigationItem from "@/shared/components/navigation-item"
 
 export default function MobileNavigation({ className }: { className?: string }) {
   return (
     <nav className={cn("bg-sidebar grid h-[52px] grid-cols-5 gap-3 px-6 pt-3 sm:h-[74px]", className)}>
-      {navigationItems.map((item) => (
-        <NavigationItem key={item.url} item={item} className="w-full" />
-      ))}
+      {navigationItems
+        .filter((item) => item.title !== "Logout")
+        .map((item) => (
+          <NavigationItem key={item.url} item={item} className="w-full" />
+        ))}
     </nav>
   )
 }

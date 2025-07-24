c669cdc9cb187729eef25834ae37fdb2302cc3a7
Justice <sherllyj191@gmail.com>
2025-07-24 08:26:16 +0100

    Moved query client to layout manager

--- src/app/layouts/index.tsx
+++ src/app/layouts/index.tsx
@@ -5,6 +5,9 @@ import { LayoutProps } from "./types"
 import AuthLayout from "./auth-layout"
 import { usePathname } from "next/navigation"
 import { routes } from "@/shared/constants/routes"
+import { queryClient } from "@/shared/lib/ts-query-client"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export default function LayoutManager({ children, defaultOpen }: LayoutProps) {
   const pathname = usePathname()
@@ -13,7 +16,12 @@ export default function LayoutManager({ children, defaultOpen }: LayoutProps) {
 
   const Layout = isAuthRoute ? AuthLayout : AppLayout
 
-  return <Layout defaultOpen={defaultOpen}>{children}</Layout>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout defaultOpen={defaultOpen}>{children}</Layout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 const authRoutes = [routes.login, routes.register]

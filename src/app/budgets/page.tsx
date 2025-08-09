508a9edb1d75eca9a2fe7b57b65e5a4ec0cc3078
Justice <sherllyj191@gmail.com>
2025-08-09 15:09:45 +0100

    UPDATE: create budget endpoint, refactorings

--- src/app/budgets/page.tsx
+++ src/app/budgets/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import React from "react"
-import { PageLayer } from "@/shared/components/page-layer"
 import Summary from "./ui/summary"
 import EnlightedCards from "./ui/enlighted-cards"
-import AddBudgets from "./ui/add-edit-budget"
+import AddBudgetDialog from "./ui/add-budget-dialog"
+import { useBudgets } from "@/shared/data/budget.data"
+import { PageLayer } from "@/shared/components/page-layer"
 
 const Summaries = [
   { color: "bg-secondary-green", name: "Entertainment", priceOf: "15.00", priceOut: "50.00", range: 90 },
@@ -27,34 +28,19 @@ const Next = [
 ]
 
 const BudgetsPage = () => {
+  const { data: budgets } = useBudgets()
+
   return (
-    <PageLayer title="Budgets" cta={<AddBudgetsCTA />} className="grid gap-6 sm:grid-cols-1 lg:grid-cols-[41.4%_58.6%]">
+    <PageLayer title="Budgets" cta={<AddBudgetDialog />} className="grid gap-6 sm:grid-cols-1 lg:grid-cols-[41.4%_58.6%]">
       <Summary summaries={Summaries} data={Data} />
 
       <div className="col-span-1 h-full w-full space-y-6">
-        {Summaries.map((value) => (
-          <EnlightedCards
-            key={value.name}
-            color={value.color}
-            name={value.name}
-            priceOf={value.priceOf}
-            priceOut={value.priceOut}
-            range={value.range}
-            next={Next}
-          />
+        {budgets.map((budget) => (
+          <EnlightedCards key={budget.category} budget={budget} next={Next} />
         ))}
       </div>
     </PageLayer>
   )
 }
-
-const AddBudgetsCTA = () => (
-  <AddBudgets
-    mode="add"
-    title="+ Add Budget"
-    description="Create a new budget to help you manage your spending effectively."
-    state="Add Budget"
-  />
-)
 
 export default BudgetsPage
---
+++ src/app/budgets/ui/add-budget-dialog.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import z from "zod"
+import React from "react"
+import { toast } from "sonner"
+import { X } from "lucide-react"
+import { cn } from "@/shared/lib/utils"
+import {
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogCancel,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogTrigger,
+} from "@/shared/components/ui/alert-dialog"
+import { Color } from "@/shared/types/color"
+import { useForm } from "@tanstack/react-form"
+import { Form } from "@/shared/components/form/form"
+import { Button } from "@/shared/components/ui/button"
+import { getErrorMessage } from "@/shared/utils/error-util"
+import { addBudgetToState } from "@/shared/data/budget.data"
+import { budgetAPI } from "@/shared/services/apis/budget.api"
+import { InputField } from "@/shared/components/form/input-field"
+import { AlertDialogDescription } from "@radix-ui/react-alert-dialog"
+import { PrefixedInputField } from "@/shared/components/form/prefixed-input-field"
+import { formValidator, isFormValid, prefixedFieldOnChange } from "@/shared/utils/form.util"
+
+const schema = z.object({
+  color: z.enum(Color, { message: "Invalid color" }),
+  category: z.string().nonempty("Category is required"),
+  maxAmount: z.object({
+    prefix: z.string().min(1, "Prefix error"),
+    value: z.number().min(0, "Maximum amount must be a positive number"),
+  }),
+})
+
+type FormData = z.infer<typeof schema>
+
+const AddBudgetDialog = () => {
+  const form = useForm({
+    defaultValues: {} as FormData,
+    validators: { onChange: formValidator(schema) },
+    onSubmit: async ({ value }) => {
+      try {
+        const resp = await budgetAPI.createBudget({
+          category: value.category,
+          color: value.color,
+          maxAmount: value.maxAmount.value,
+        })
+        addBudgetToState(resp.data)
+        toast.success(resp.message)
+      } catch (error) {
+        toast.error("Failed to create budget", { description: getErrorMessage(error) })
+      }
+    },
+  })
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className={cn("btn btn-default btn-size-lg")}>+ Add Budget</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader className="flex w-full flex-row items-center justify-between">
+          <AlertDialogTitle className="sm:text-preset-1 text-gray-900">Add New Budget</AlertDialogTitle>
+
+          <AlertDialogCancel className="max-h-8 max-w-8 rounded-full border-gray-500 p-0">
+            <X className="size-4 text-gray-500" />
+          </AlertDialogCancel>
+        </AlertDialogHeader>
+
+        <AlertDialogDescription className="text-preset-4 text-gray-500">
+          Choose a category to set a spending budget. These categories can help you monitor spending.
+        </AlertDialogDescription>
+
+        <Form className="grid gap-4">
+          <form.Field name="category" children={(field) => <InputField field={field} label="Budget Category" />} />
+
+          <form.Field
+            name="maxAmount"
+            defaultValue={{ prefix: "$", value: 0 }}
+            validators={{
+              onChangeListenTo: ["maxAmount.prefix", "maxAmount.value"],
+              onChange: (e) => prefixedFieldOnChange(schema.pick({ maxAmount: true }), { maxAmount: e.value }, e.fieldApi),
+            }}
+            children={(field) => (
+              <PrefixedInputField
+                label="Maximum Spend"
+                form={form}
+                field={field}
+                prefixProps={{ name: "maxAmount.prefix" }}
+                valueProps={{ name: "maxAmount.value", onChange: (e) => parseInt(e.target.value) }}
+              />
+            )}
+          />
+
+          <form.Field name="color" children={(field) => <InputField field={field} label="Theme" />} />
+
+          <AlertDialogFooter className="h-[3.32rem]">
+            <form.Subscribe
+              selector={({ fieldMeta, isSubmitting }) => ({ isValid: isFormValid(fieldMeta), isSubmitting })}
+              children={({ isValid, isSubmitting }) => {
+                return (
+                  <Button
+                    type="submit"
+                    size="lg"
+                    className="w-full"
+                    disabled={!isValid}
+                    isLoading={isSubmitting}
+                    onClick={form.handleSubmit}
+                  >
+                    Add Budget
+                  </Button>
+                )
+              }}
+            />
+          </AlertDialogFooter>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+export default AddBudgetDialog
